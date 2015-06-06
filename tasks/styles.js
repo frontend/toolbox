e24e581a0364612d1c17228adcb6aca10fe9dfc8
Yann Gouffon <github@yago-art.ch>
2015-06-06 13:18:55 +0200

    bootstrap site with generator-toolbox 🔧

---
+++ tasks/styles.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var gulp          = require('gulp'),
+    $             = require('gulp-load-plugins')(),
+    config        = require('../gulp_config.json'),
+    argv          = require('yargs').argv,
+    slug          = require('slug');
+
+module.exports = function() {
+
+  var iconFontName = slug(config.iconsFontName).toLowerCase();
+
+  /**
+   * Build styles from SCSS files
+   * With error reporting on compiling (so that there's no crash)
+   */
+  gulp.task('styles', function() {
+    if (argv.production) { console.log('[styles] Production mode' ); }
+    else { console.log('[styles] Dev mode'); }
+
+    gulp.src([config.assets + 'sass/' + iconFontName + '.scss', config.assets + 'sass/main.scss'])
+      .pipe($.if(!argv.production, $.sourcemaps.init()))
+      .pipe($.sass({
+        outputStyle: 'nested', // libsass doesn't support expanded yet
+        precision: 10,
+        includePaths: ['.']
+      }).on('error', $.sass.logError))
+      .pipe($.postcss([
+        require('autoprefixer-core')({
+          browsers: config.browsers,
+          options: {
+            map: true
+          }
+        })
+      ]))
+      .pipe($.if(!argv.production, $.sourcemaps.write()))
+      .pipe($.if(argv.production, $.minifyCss()))
+      .pipe($.concat('main.css'))
+      .pipe($.size({title: 'STYLES', showFiles: true}))
+      .pipe(gulp.dest(config.build + '/css'));
+  });
+
+};
