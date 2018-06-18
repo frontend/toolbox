d7c80d7020291cae9f851a08c11a736562c9908e
Yann Gouffon <github@yago-art.ch>
2018-06-18 10:53:11 +0200

    🚀 fix deploy scripts

--- .bin/deploy.js
+++
@@ -1,3 +0,0 @@
-const ghpages = require('gh-pages');
-
-ghpages.publish('_book', (err) => { console.error(err); });
--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -1,32 +1,42 @@
- #!/bin/sh
+#!/bin/sh
 
-DIR=$(dirname "$0")
+DIRECTORY="_book"
+BRANCH="gh-pages"
+CURRENT_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-cd $DIR/..
-
-if [[ $(git status -s) ]]
+# Check if the environment is ready for publishing ===========================
+if [ "$CURRENT_BRANCH" != "master" ]
 then
-    echo "The working directory is dirty. Please commit any pending changes."
+    echo "⚠️  Please run this script from master branch"
     exit 1;
 fi
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
+if [[ $(git status -s) ]]
+then
+    echo "⚠️  The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+# Proceed =====================================================================
+echo "backup dist content"
+mkdir "$DIRECTORY-tmp"
+cp -r $DIRECTORY/* "$DIRECTORY-tmp/"
+
+echo "Deleting dist"
+rm -rf $DIRECTORY
+mkdir $DIRECTORY
+git worktree prune
+rm -rf .git/worktrees/$DIRECTORY/
+
+echo "Checking out $BRANCH branch into dist"
+git worktree add -B $BRANCH $DIRECTORY
 
 echo "Removing existing files"
-rm -rf public/*
+rm -rf $DIRECTORY/*
 
-echo "Generating site"
-yarn build
+echo "Generating dist using the backup"
+cp -r "$DIRECTORY-tmp"/* $DIRECTORY/
+rm -rf "$DIRECTORY-tmp"
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (deploy.sh)"
-
-echo "Pushing gh-pages branch"
-git push origin gh-pages
+echo "Updating $BRANCH branch"
+cd $DIRECTORY && git add --all && git commit -m "Publishing to $BRANCH (deploy.sh)" git push --force origin $BRANCH --tags
